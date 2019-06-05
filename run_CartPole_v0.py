6314c346335f498831d498ffb63577a57f02b115
darthegg <darthegg21@gmail.com>
2019-06-05 20:20:33 +0900

    fix train loop and tun hyperparameter

--- run_CartPole_v0.py
+++ run_CartPole_v0.py
@@ -13,18 +13,37 @@ import numpy as np
 from model.dqn import DQN
 from buffer.replaybuffer import ReplayBuffer
 
-
+"""
+# ep 500
 BUFFER_SIZE = 10000
-BATCH_SIZE = 128
+BATCH_SIZE = 32
 GAMMA = 0.99
-epsilon = 0.01
+LEARNING_RATE = 0.0003
+epsilon = 1.0
 max_epsilon = 1.0
 min_epsilon = 0.01
-epsilon_decay = 0.0001
-EPOCH = 20
+epsilon_decay = 0.002
 
-MAX_EPISODE = 5
-MAX_STEP = 500
+# PRE_EPISODE = 1
+MAX_STEP = 100
+MAX_EPISODE = 10000
+TARGET_UPDATE_EP = 20
+"""
+
+BUFFER_SIZE = 10000
+BATCH_SIZE = 32
+GAMMA = 0.99
+LEARNING_RATE = 0.0003
+epsilon = 1.0
+max_epsilon = 1.0
+min_epsilon = 0.01
+epsilon_decay = 0.002
+
+# PRE_EPISODE = 1
+MAX_STEP = 100
+MAX_EPISODE = 10000
+TARGET_UPDATE_EP = 20
+
 
 
 # Env initialize
@@ -35,53 +54,75 @@ state = env.reset()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 memory = ReplayBuffer(BUFFER_SIZE, BATCH_SIZE, device)
 model = DQN(env, device)
+model_target = DQN(env, device)
 
 if torch.cuda.is_available():
     model = model.cuda()
+    model_target = model_target.cuda()
 
-optimizer = optim.Adam(model.parameters())
-
-reward_log = np.zeros((MAX_STEP, 1))
-episode = 0
+model_target.load_state_dict(model.state_dict())
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 
-while True:
-    for _ in range(MAX_EPISODE):
-        for i in range(MAX_STEP):
-            env.render()
-            action = model.select_action(state, epsilon, 'train')
-            next_state, reward, done, _ = env.step(action)
-            memory.add(state, action, reward, next_state, done)
-            state = next_state
-            reward_log[i] = reward
-            if done is True:
-                break
-        state = env.reset()
+def get_td_loss(memory, model, model_target, GAMMA):
+    states_, actions_, rewards_, next_states_, dones_ = memory.sample()
 
-        epsilon = max(epsilon - (max_epsilon - min_epsilon) * epsilon_decay, min_epsilon)
-        episode = episode + 1
+    next_max_q = model_target.get_max_q(next_states_)
+    measured_q = rewards_ + GAMMA * next_max_q * (1 - dones_)
 
-        print("episode : ", episode,  "reward average : ", np.average(reward_log))
+    q_set = model.forward_tensor(states_)
+    estimated_q = q_set[torch.arange(q_set.size(0)), actions_]
 
-        reward_log = np.zeros((MAX_STEP, 1))
+    loss = (measured_q - estimated_q).pow(2).mean()
 
-    model_target = model
+    return loss
 
-    for _ in range(EPOCH):
-        states_, actions_, rewards_, next_states_, dones_ = memory.sample()
+"""
+for _ in range(PRE_EPISODE):
+    while True:
+        # env.render()
+        
+        action = model.select_action(state, epsilon, 'test')
+        next_state, reward, done, _ = env.step(action)
+        memory.add(state, action, reward, next_state, done)
+        state = next_state
+        if done is True:
+            break
+    state = env.reset()
+"""
 
-        next_max_q = model_target.get_max_q(next_states_)
-        measured_q = rewards_ + GAMMA * next_max_q * (1 - dones_)
-
-        q_set = model.forward_tensor(states_)
-        estimated_q = q_set[torch.arange(q_set.size(0)), actions_]
-
-        loss = (measured_q - estimated_q).pow(2).mean()
+for episode in range(MAX_EPISODE):
+    reward_log = np.array([])
+    t = 0
+    while True:
+        #env.render()
+        
+        action = model.select_action(state, epsilon, 'train')
+        next_state, reward, done, _ = env.step(action)
+        memory.add(state, action, reward, next_state, done)
+        state = next_state
+        reward_log = np.append(reward_log, reward)
+        
+        loss = get_td_loss(memory, model, model_target, GAMMA)
         # print("Loss : ", loss.item())
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+
+        t = t + 1
+
+        if done is True:
+            break
+
+    if episode % TARGET_UPDATE_EP == 0:
+        model_target.load_state_dict(model.state_dict())
+
+    epsilon = max(epsilon - (max_epsilon - min_epsilon) * epsilon_decay, min_epsilon)
+    print("episode : ", episode,  "reward sum : ", np.average(reward_log)*t/100, "Epsilon : ", epsilon)
+
+    state = env.reset()
+
 
     # TODO
     # save and load model
